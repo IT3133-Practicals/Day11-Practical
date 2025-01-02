2baf105751f25fa15c7500a5c4f1f76bfacabc02
madurangaPrabhath <maduracourse@gmail.com>
2025-01-02 23:07:44 +0530

    Add CSS part to Home component

--- MyMobileApp/App.js
+++ MyMobileApp/App.js
@@ -1,6 +1,5 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { PaperProvider } from 'react-native-paper';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import Home from './components/Home';
