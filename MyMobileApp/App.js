199c1243ebd579d3c419a5f0e6835580a6a3fb7e
madurangaPrabhath <maduracourse@gmail.com>
2025-01-02 15:40:30 +0530

    Import React NativeStackNavigator to App.js

--- MyMobileApp/App.js
+++ MyMobileApp/App.js
@@ -1,15 +1,22 @@
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { PaperProvider } from 'react-native-paper';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
+const Stack = createNativeStackNavigator();
+
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
   return (
     <SafeAreaProvider>
       <NavigationContainer>
-        <View style={styles.container}>
-          <Text>Open up App.js to start working on your app!</Text>
-          <StatusBar style="auto" />
-        </View>
+        <Stack.Navigator>
+          <View style={styles.container}>
+            <Text>Open up App.js to start working on your app!</Text>
+            <StatusBar style="auto" />
+          </View>
+        </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
   );
