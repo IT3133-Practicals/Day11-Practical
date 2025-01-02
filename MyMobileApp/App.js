dd6a5a87af94363e376f1bc014c8a7eccdc3335b
madurangaPrabhath <maduracourse@gmail.com>
2025-01-02 15:36:14 +0530

    Import React Native Navigation to project

--- MyMobileApp/App.js
+++ MyMobileApp/App.js
@@ -1,15 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 import { PaperProvider } from 'react-native-paper';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <NavigationContainer>
+        <View style={styles.container}>
+          <Text>Open up App.js to start working on your app!</Text>
+          <StatusBar style="auto" />
+        </View>
+      </NavigationContainer>
     </SafeAreaProvider>
   );
 }
